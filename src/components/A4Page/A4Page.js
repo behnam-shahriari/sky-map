41ac5c0c488c3786c1cb100c88e98899526cd3a9
Behnam Shahriari <shahriaribehnam@gmail.com>
2022-04-21 23:21:50 +0300

    A4Page component is created

---
+++ src/components/A4Page/A4Page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './A4page.css';
+
+const A4Page = () => {
+    return(
+        <div className="container">
+          <div className="wrapped">
+          </div>
+        </div>
+    ) 
+}
+
+export default A4Page;
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import A4Page from "../components/A4Page/A4Page";
+
+function App() {
+  return (
+    <div>
+      <A4Page />
+    </div>
+  );
+}
+
+export default App;
