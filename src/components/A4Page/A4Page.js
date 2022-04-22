eba1e142e991b5d48991b3b92e40a52910ecf548
Behnam Shahriari <shahriaribehnam@gmail.com>
2022-04-22 13:24:39 +0300

    React-Virtual-Sky is added

--- src/components/A4Page/A4Page.js
+++ src/components/A4Page/A4Page.js
@@ -1,13 +1,40 @@
-import React from "react";
-import './A4page.css';
+import React, { Component } from "react";
+import VirtualSky from "react-virtual-sky/dist/VirtualSky/VirtualSky";
 
-const A4Page = () => {
-    return(
-        <div className="container">
-          <div className="wrapped">
-          </div>
-        </div>
-    ) 
+
+export default class A4Page extends Component {
+    config = {
+        azOff: 0,
+        height: 500,
+        width: 500,
+        latitude: 51.746449,
+        longitude: 19.620693,
+        time: new Date(),
+        skyColors: [ "#000", "#100050" ],
+        gridAzColor: "#100050",
+        gridEqColor: "#105000",
+        gridGalColor: "#500020",
+        language: 'en',
+        visibility:{
+          starMag: 6,
+          showStarLabels: false,
+          showPlanets: true,
+          showPlanetsOrbit: true,
+          showPlanetsLabels: false,
+          showSunMoon: true,
+          showConstellations: true,
+          showConstellationBoundaries: false,
+          showConstellationLabels: false,
+          showAzLabels: true,
+          showAzGrid: true,
+          showEqGrid: true,
+          showGalGrid: false,
+          showGalaxy: true,
+          showInfo: true
+        }
+      }
+
+    render() {
+        return <VirtualSky id="startmap" config={this.config}/>
+      }
 }
-
-export default A4Page;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,15 @@
+import React, { Component } from "react";
 import "./App.css";
 import A4Page from "../components/A4Page/A4Page";
 
-function App() {
-  return (
-    <div>
-      <A4Page />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <A4Page />
+      </div>
+    );
+  }
 }
 
 export default App;
